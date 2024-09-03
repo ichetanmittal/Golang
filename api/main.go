9fd3eca219cdd6803d999bb77d7adf47f43fd610
Chetan Mittal <chetanm.it.22@nitj.ac.in>
2024-09-04 00:18:00 +0530

    CRUD initialized

--- api/main.go
+++ api/main.go
@@ -4,9 +4,9 @@ import (
 	"fmt"
 	"net/http"
 	"encoding/json"
-
+	"strconv"
+	"math/rand"
 )
-
 
 // Model for course - file
 type Course struct {
@@ -29,13 +29,11 @@ func (c *Course) IsEmpty() bool {
 	return c.CourseID == "" && c.CourseName == ""
 }
 
-
 func main() {
 	fmt.Println("Hello World")
 }
 
 // controllers - file
-
 
 // serve home route
 
@@ -47,4 +45,43 @@ func getAllCourses(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Get all courses")
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(courses)
+}
+
+func getOneCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Get one course")
+	w.Header().Set("Content-Type", "application/json")
+
+	params := mux.Vars(r)
+	for _, course := range courses {
+		if course.CourseID == params["courseid"] {
+			json.NewEncoder(w).Encode(course)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode("No course found with the given id")
+	return
+}
+
+func createOneCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Create one course")
+	w.Header().Set("Content-Type", "application/json")
+
+	// what if: body is empty
+	if r.Body == nil {
+		json.NewEncoder(w).Encode("No course data found")
+	}
+
+	// what about - {}
+	var course Course
+	_ = json.NewDecoder(r.Body).Decode(&course)
+	if course.IsEmpty() {
+		json.NewEncoder(w).Encode("No course data found")
+	}
+
+	// generate a unique id, string
+	// append course into courses
+
+	course.CourseID = strconv.Itoa(rand.Intn(100))
+	courses = append(courses, course)
+	json.NewEncoder(w).Encode(course)
 }
